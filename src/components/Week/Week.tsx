043b656dbc6937d739e3bd6cf765d4aac6402a8f
Akahige <hello@manneta.com>
2023-07-17 22:10:50 +0100

    add weekClassName

--- src/components/Week/Week.tsx
+++ src/components/Week/Week.tsx
@@ -5,9 +5,9 @@ import style from "./Week.module.css";
 import useProps from "../../hooks/useProps";
 
 function Week({ days }: WeekProps) {
-  const { format, dayElement } = useProps();
+  const { format, dayElement, weekClassName } = useProps();
   return (
-    <div className={style.week}>
+    <div className={style.week + " " + weekClassName}>
       {days.map((d: string, j: number) => (
         <SliceProvider key={d} selector={daySelector(d, j)}>
           {dayElement || new Chronos(d, format).format("DD")}
