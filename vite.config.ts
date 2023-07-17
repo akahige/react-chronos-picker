336bff97200e0add9a2402bdfa1d89d0d1e0d19d
Akahige <hello@manneta.com>
2023-07-17 21:05:20 +0100

    try css injection plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 // import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
-    cssCodeSplit: true,
     lib: {
       entry: "src/index.ts",
       name: "ReactChronosPicker",
